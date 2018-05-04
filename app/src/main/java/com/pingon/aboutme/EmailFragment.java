40c0cc1745fd335be5cf877785c0cac4ede12517
nicoundu <38764619+nicoundu@users.noreply.github.com>
2018-05-04 16:14:00 -0300

    Initial commit

---
+++ app/src/main/java/com/pingon/aboutme/EmailFragment.java
@@ -0,0 +1,50 @@
+package com.pingon.aboutme;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EmailFragment extends Fragment {
+
+
+    public EmailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_email, container, false);
+            }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button contactBtn = (Button) view.findViewById(R.id.contactBtn);
+        //no me exige castear para que funcione la app, no me arroja error
+        contactBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Funciono", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+    }
+
+
+}
