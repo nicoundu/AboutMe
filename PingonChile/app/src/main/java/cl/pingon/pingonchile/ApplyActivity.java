17c4ca774bacc8aefd43bbb9910dcf1b59759881
nicoundu <38764619+nicoundu@users.noreply.github.com>
2018-05-16 08:57:38 -0400

    commit

--- PingonChile/app/src/main/java/cl/pingon/pingonchile/ApplyActivity.java
+++ PingonChile/app/src/main/java/cl/pingon/pingonchile/ApplyActivity.java
@@ -2,6 +2,8 @@ package cl.pingon.pingonchile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ApplyActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class ApplyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apply);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.apply_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
